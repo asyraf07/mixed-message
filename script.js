d0b77a3688a0a64eeeff6732bb1f78a7913194c1
asyraf07 <asyraf.sr07@gmail.com>
2021-01-01 16:30:50 +0700

    Update script.js documentation

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-/*
-Output:
-The weather is : ["Rainy", "Cloudy", "Sunny", "Stormy"]
-It means you should : ["go outside and chillin", "Use your best Sweater", "Take a nap", "Sleeping through the day", "Choose your best sunglasses to wear"]
-With: ["Your friends", "your Family", "yourself", "your girlfriend"]
-*/
-
+// Randomizer -> pick random elemetn inside the array
 const randomPick = arr => {
      const rand = Math.floor(Math.random() * arr.length);
      return arr[rand];
 };
+
+// Pick random weather
 const randomWeather = () => randomPick(["rainy", "cloudy", "sunny", "stormy"]);
+
+// Pick random advice message
 const randomAdvice = () => randomPick(["go outside and chillin", "use your best sweater", "take a nap", "sleeping through the day", "choose your best sunglasses to wear"]);
+
+// Pick random partner
 const randomPartner = () => randomPick(["your friends", "your family", "yourself", "your girlfriend"]);
 
+// Combine and display the message
 const run = () => {
      console.log(`The weather is ${randomWeather()},`);
      console.log(`that means you should ${randomAdvice()}`);
@@ -20,3 +21,9 @@ const run = () => {
 }
 
 run();
+/*
+Output:
+The weather is : ["Rainy", "Cloudy", "Sunny", "Stormy"]
+It means you should : ["go outside and chillin", "Use your best Sweater", "Take a nap", "Sleeping through the day", "Choose your best sunglasses to wear"]
+With: ["Your friends", "your Family", "yourself", "your girlfriend"]
+*/
